94050b433937c35653c6bbdf7ea2c2cc4747b199
Noel Paing Oak Soe <u6530183@au.edu>
2024-11-08 23:32:52 +0700

    product endpoint

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,6 +1,6 @@
 import Product from '../models/product.model'
 import { client } from '../lib/redis'
-import cloudinary from 'cloudinary'
+import cloudinary from '../lib/cloudinary.js'
 
 const getAllProducts = async (req, res) => {
   try {
@@ -29,7 +29,7 @@ export const getFeaturedProducts = async (req, res) => {
       return res.json(JSON.parse(featuredProducts))
     }
 
-    const products = await Product.find({ isFeatured: true })
+    const products = await Product.find({ isFeatured: true }).lean()
 
     if (!products) {
       return res.status(404).json({ message: 'No featured products found' })
@@ -43,3 +43,29 @@ export const getFeaturedProducts = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: error.message })
   }
 }
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category, isFeatured } = req.body
+
+    if (image) {
+      const uploadedImage = await cloudinary.uploader.upload(image, {
+        folder: 'products',
+      })
+    }
+    const product = Product.create({
+      name,
+      description,
+      price,
+      image: uploadedImage?.secure_url ? uploadedImage.secure_url : '',
+      category,
+    })
+
+    res.status(201).json(product)
+  } catch (error) {
+    console.log('Error in createProduct', error.message)
+    res.status(500).json({ message: 'Server error', error: error.message })
+  }
+}
+
+export const deleteProduct = async (req, res) => {}
