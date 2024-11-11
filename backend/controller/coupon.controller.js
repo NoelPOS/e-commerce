e3a57c8f6f5912e3d4523d153b24a0974296f6fc
Noel Paing Oak Soe <u6530183@au.edu>
2024-11-11 20:07:59 +0700

    add coupon and cart endpoints

--- backend/controller/coupon.controller.js
+++ backend/controller/coupon.controller.js
@@ -10,9 +10,35 @@ export const getCoupon = async (req, res) => {
   }
 }
 
-// export const validateCoupon = async (req, res) => {
-//     try{
+export const validateCoupon = async (req, res) => {
+  try {
+    const { code } = req.body
 
-//     }
+    const coupon = await Coupon.findOne({
+      code: code,
+      isActive: true,
+      userId: req.user._id,
+    })
 
-// }
+    if (!coupon) {
+      return res.status(404).json({ message: 'Coupon not found' })
+    }
+
+    if (coupon.expirationDate < new Date()) {
+      coupon.isActive = false
+      await Coupon.save()
+      return res.status(404).json({ message: 'Coupon expired' })
+    }
+
+    res
+      .status(200)
+      .json({
+        message: 'coupon validated',
+        code: coupon.code,
+        discountPercentage: coupon.discountPercentage,
+      })
+  } catch (error) {
+    console.log('Error in validateCoupon', error.message)
+    res.status(500).json({ message: 'Server error', error: error.message })
+  }
+}
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './route/auth.route.js'
 import productRoutes from './route/product.route.js'
 import couponRoutes from './route/coupon.route.js'
+import cartRoutes from './route/cart.route.js'
 
 import { connectDB } from './lib/db.js'
 
@@ -19,6 +20,7 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/coupons', couponRoutes)
+app.use('/api/cart', cartRoutes)
 // app.use('/api/users', userRoutes)
 
 app.listen(PORT, () => {
